c86a8a64859b27821e2232ffd62bc4fa4ee98202
irina <1486202@gmail.com>
2020-04-25 20:23:19 +0300

    Разделила на компоненты

---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR4sOC3EDn5KiYoJIBvDvegFUFXI1zGRxQP2xI3BQPrGR4qYmd8&usqp=CAU'/>
+    </header>
+}
+export default Header;
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Nav = () => {
+    return <nav className='nav'>
+        <div>
+            <a>First glass</a>
+        </div>
+        <div>
+            <a>Second glass</a>
+        </div>
+        <div>
+            <a>Third glass</a>
+        </div>
+        <div>
+            <a>Fourth and more</a>
+        </div>
+    </nav>
+}
+export default Nav;
