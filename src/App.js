ea9cb24610ab6077762e5654ec0f7230d87285e8
irina <1486202@gmail.com>
2020-04-25 19:38:31 +0300

    Сверстала главную страницу

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import './App.css';
+
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR4sOC3EDn5KiYoJIBvDvegFUFXI1zGRxQP2xI3BQPrGR4qYmd8&usqp=CAU'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>First glass</a>
+                </div>
+                <div>
+                    <a>Second glass</a>
+                </div>
+                <div>
+                    <a>Third glass</a>
+                </div>
+                <div>
+                    <a>Fourth and more</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                <img
+                    src='https://lh3.googleusercontent.com/proxy/eOdOiIU1AE6NuzAcEBg925lzZJd51FQiS6SOtfPkg3zJ5O_IYJjhSdEKQzHIAtJF8v3UYLd_CtCSzFIQaYy9P2yv66ojveqOeLf5ijGhQul9i3w3tDM2tf2VcUaBSRXZRArkbRtf3kxc77BU1um8eNxJZocBUvMQCg'/>
+                <img
+                    src='https://lh3.googleusercontent.com/proxy/eOdOiIU1AE6NuzAcEBg925lzZJd51FQiS6SOtfPkg3zJ5O_IYJjhSdEKQzHIAtJF8v3UYLd_CtCSzFIQaYy9P2yv66ojveqOeLf5ijGhQul9i3w3tDM2tf2VcUaBSRXZRArkbRtf3kxc77BU1um8eNxJZocBUvMQCg'/>
+                </div>
+            <div>
+                ava
+            </div>
+                <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>)
+
+
+};
+
+
+export default App;
