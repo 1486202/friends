0efaf319ae85a0ad8eba25524b81235230940fbe
irina <1486202@gmail.com>
2020-04-26 20:10:19 +0300

    Меню навбар разноцветное, улучшены ссылки.

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import s from './Nav.module.css';
+import {NavLink} from "react-router-dom";
+
 const Nav = () => {
-    return <nav className={s.nav}>
+    return (
+        <nav className={s.nav}>
         <div className={s.item}>
-            <a href="/content">Profile</a>
+            <NavLink to="/content" activeClassName={s.active}>Profile</NavLink>
         </div>
         <div className={s.item}>
-            <a href="/dialogs">Dialogs</a>
+            <NavLink to="/dialogs" activeClassName={s.active}>Dialogs</NavLink>
         </div>
         <div className={s.item}>
-            <a href="/first">First glass</a>
+            <NavLink to="/first" activeClassName={s.active}>First glass</NavLink>
         </div>
         <div className={s.item}>
-            <a href="/fourth">Fourth and more</a>
+            <NavLink to="/fourth" activeClassName={s.active}>Fourth and more</NavLink>
         </div>
-    </nav>
+    </nav>)
 }
 export default Nav;
