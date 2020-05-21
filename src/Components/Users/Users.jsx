b6a49a4ca0563e3a681c66301d9c96bd56a69fce
irina <1486202@gmail.com>
2020-05-21 22:05:08 +0300

    Сделала запрос на сервер, получили пользователей.

--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -1,56 +1,44 @@
 import React from "react";
 import styles from './users.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/images/2ebea45c2018eb963efb22fce3a909ed.png';
+
 let Users = (props) => {
-    if (props.users.length === 0){
-    props.setUsers([
-        {
-            id: 1,
-            photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
-            followed: false,
-            fullName: 'Lexa',
-            status: 'I am good lawyer',
-            location: {city: 'Astrakhan', country: 'Russia'}
-        },
-        {
-            id: 2,
-            photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
-            followed: true,
-            fullName: 'Andrew',
-            status: 'I have a three cats',
-            location: {city: 'Moscow', country: 'Russia'}
-        },
-        {
-            id: 3,
-            photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
-            followed: false,
-            fullName: 'Denis',
-            status: 'I am fell in love',
-            location: {city: 'Kiev', country: 'Ukraine'}
-        },
-    ])};
-    return <div> {
-        props.users.map(u => <div key={u.id}>
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                });
+        }
+        ;
+        return <div> {
+            props.users.map(u => <div key={u.id}>
         <span>
             <div>
-                <img src={u.photoUrl} className={styles.userPhoto}/>
+                <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
             </div>
             <div>{u.followed
-                ? <button onClick={ () => {props.unfollow(u.id)}}>Unfollow</button>:
-                <button onClick={ () => {props.follow(u.id)}}>Follow</button>}
+                ? <button onClick={() => {
+                    props.unfollow(u.id)
+                }}>Unfollow</button> :
+                <button onClick={() => {
+                    props.follow(u.id)
+                }}>Follow</button>}
             </div>
         </span>
-        <span>
+                <span>
             <span>
-                <div>{u.fullName}</div>
+                <div>{u.Name}</div>
                 <div>{u.status}</div>
             </span>
             <span>
-                 <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+                 <div>{"u.location.country"}</div>
+                <div>{"u.location.city"}</div>
             </span>
         </span>
-         </div>)
+            </div>)
+        }
+        </div>
     }
-    </div>
-};
+;
 export default Users;
--- src/redux/users_reducer.js
+++ src/redux/users_reducer.js
@@ -3,30 +3,30 @@ const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS'
 let initialState = {
     users: [
-        {
-            id: 1,
-            photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
-            followed: false,
-            fullName: 'Lexa',
-            status: 'I am good lawyer',
-            location: {city: 'Astrakhan', country: 'Russia'}
-        },
-        {
-            id: 2,
-            photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
-            followed: true,
-            fullName: 'Andrew',
-            status: 'I have a three cats',
-            location: {city: 'Moscow', country: 'Russia'}
-        },
-        {
-            id: 3,
-            photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
-            followed: false,
-            fullName: 'Denis',
-            status: 'I am fell in love',
-            location: {city: 'Kiev', country: 'Ukraine'}
-        },
+        // {
+        //     id: 1,
+        //     photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
+        //     followed: false,
+        //     fullName: 'Lexa',
+        //     status: 'I am good lawyer',
+        //     location: {city: 'Astrakhan', country: 'Russia'}
+        // },
+        // {
+        //     id: 2,
+        //     photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
+        //     followed: true,
+        //     fullName: 'Andrew',
+        //     status: 'I have a three cats',
+        //     location: {city: 'Moscow', country: 'Russia'}
+        // },
+        // {
+        //     id: 3,
+        //     photoUrl: 'https://hornews.com/upload/images/blank-avatar.jpg',
+        //     followed: false,
+        //     fullName: 'Denis',
+        //     status: 'I am fell in love',
+        //     location: {city: 'Kiev', country: 'Ukraine'}
+        // },
     ],
 };
 const usersReducer = (state = initialState, action) => {
