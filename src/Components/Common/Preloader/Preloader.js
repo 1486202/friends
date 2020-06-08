07d73d22556925c6f673ed157ece38e9073d173a
irina <1486202@gmail.com>
2020-06-08 20:17:51 +0300

    Preloader и изменила mapdispatch в userconteiner

---
+++ src/Components/Common/Preloader/Preloader.js
@@ -0,0 +1,7 @@
+import preloader from "../../../assets/images/Bean Eater-0.7s-157px.svg";
+import React from "react";
+
+let Preloader = (props) => {
+    return <img src={preloader}/>
+}
+export default Preloader;
